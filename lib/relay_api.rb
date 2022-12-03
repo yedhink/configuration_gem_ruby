65c90ed079fc41662a2584959b63d6a63b2394d3
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2022-12-03 08:56:30 +0530

    first commit

---
+++ lib/relay_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "relay_api/version"
+
+module RelayApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/relay_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RelayApi
+  VERSION = "0.1.0"
+end
